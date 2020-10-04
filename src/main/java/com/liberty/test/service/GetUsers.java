904c48723c5f1e715509bd899658bacc3543adb6
Ravella90 <72326207+Ravella90@users.noreply.github.com>
2020-10-03 22:54:36 -0400

    Update GetUsers.java
    Format

--- src/main/java/com/liberty/test/service/GetUsers.java
+++ src/main/java/com/liberty/test/service/GetUsers.java
@@ -1,72 +1,39 @@
 package com.liberty.test.service;
 
-import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.liberty.test.model.User;
-import com.liberty.test.request.UserRequest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Service
 public class GetUsers {
 
-
-
-//
-//	@Test
-//	public void getUsers() {
-//
-//		User user = ServiceCall(null);
-//		assertEquals(null, user.getName());
-//		assertEquals(null, user.getJob());
-//		assertEquals("1", user.getPage());
-//		assertEquals("6", user.getPer_page());
-//		assertEquals("george.bluth@reqres.in", user.getData().get(0).getEmail());
-//		assertEquals("janet.weaver@reqres.in", user.getData().get(1).getEmail());
-//		assertEquals("6", user.getPer_page());
-//		assertEquals("6", user.getPer_page());
-//
-//	}
-//
-//	@Test
-//	public void postUsers() {
-//
-//		UserRequest userRequest = new UserRequest();
-//		userRequest.setName("darth vader");
-//		userRequest.setJob("villain");
-//		User user = ServiceCall(userRequest);
-//		assertEquals("darth vader", user.getName());
-//		assertEquals("villain", user.getJob());
-//		assertEquals("1", user.getPage());
-//		assertEquals("6", user.getPer_page());
-//		assertEquals("tracey.ramos@reqres.in", user.getData().get(5).getEmail());
-//		assertEquals("StatusCode Weekly", user.getAd().getCompany());
-//
-//	}
-
-
-
+	public final static String YES = "YES";
 	private RestTemplate restTemplate = new RestTemplate();
 
-	public User getUsersList( String userAgent) {
+	public User getUsersList(String userAgent) {
 		String uri = "https://reqres.in/api/users";
 		HttpHeaders headers = new HttpHeaders();
-//		if (userAgent.equalsIgnoreCase("yes")) {
-//			headers.add("user-agent",
-//					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-//		}
+		if (userAgent.equalsIgnoreCase(YES)) {
+			headers.add("user-agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		}
 		HttpEntity<?> entity = new HttpEntity<>(headers);
 		ResponseEntity<User> response = restTemplate.exchange(uri, HttpMethod.GET, entity, User.class);
 
 		return response.getBody();
 	}
 
-
-	public User postUserInfo(String name, String job) throws Exception {
+	public Person postUserInfo(String name, String job) throws Exception {
 		String uri = "https://reqres.in/api/users";
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
@@ -74,16 +41,16 @@ public class GetUsers {
 				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 		HttpEntity<?> entity = new HttpEntity<>(headers);
 		Person person = new Person(name, job);
-
-
-		return restTemplate.postForObject(uri, person, User.class);
+		ResponseEntity<Person> response = restTemplate.exchange(uri, HttpMethod.POST, entity, Person.class);
+		return response.getBody();
 
 	}
 }
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 class Person {
-private String name;
-private String job;
+	private String name;
+	private String job;
 }
