49a39aa2f9257c71499b7fe58ab27b014fc90629
Ravella90 <72326207+Ravella90@users.noreply.github.com>
2020-10-03 22:52:20 -0400

    Update ArgumentException.java
    Format

--- src/main/java/com/liberty/test/error/ArgumentException.java
+++ src/main/java/com/liberty/test/error/ArgumentException.java
@@ -7,14 +7,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class ArgumentException extends RuntimeException {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private final double amount;
-	@NonNull private final String errorMessage;
-	
-	
+	@NonNull
+	private final String errorMessage;
+
 }
